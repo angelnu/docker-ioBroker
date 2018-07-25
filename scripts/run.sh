5f047c7eac130793f64d7e075601c424c2981f4b
Vegetto <github@angelnu.com>
2018-07-25 08:26:15 +0200

    Update method for setting time zone

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,12 +2,9 @@
 
 IOBROKER_CMD="node node_modules/iobroker.js-controller/controller.js $*"
 
-echo "Set timezone"
-cp -av /etc/timezone_host /etc/timezone
-#Line for Alpine (no glibc)
-cp /usr/share/zoneinfo/$(cat /etc/timezone) /etc/localtime
-# Line for Ubuntu
-# timedatectl set-timezone $(cat /etc/timezone)
+echo "Set localtime"
+cp -av /etc/localtime_host /etc/localtime
+dpkg-reconfigure -f noninteractive tzdata
 
 echo "Execute setup"
 ./iobroker setup
