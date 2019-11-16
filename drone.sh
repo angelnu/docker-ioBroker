b05561ad10bae1ce91c6527d39e179d3bd39d2d5
angelnu <github@angelnu.com>
2019-11-16 19:52:57 +0100

    Support other archs

--- drone.sh
+++ drone.sh
@@ -15,6 +15,22 @@ DRONE_OPTS="--secret-file secrets.yml \
 export DRONE_COMMIT=$GIT_COMMIT
 export DRONE_BUILD_NUMBER=99999
 
+
+case "$(uname -m)" in
+    x86_64)
+        ARCH=amd64
+        ;;
+    armv7l)
+        ARCH=arm
+        ;;
+    aarch64)
+        ARCH=arm64
+        ;;
+    *)
+        echo "Uknown architecture!"
+        ARCH=$(uname -m)
+esac
+
 drone jsonnet --stream
 drone exec $DRONE_OPTS --pipeline build_full-amd64
 drone exec $DRONE_OPTS --pipeline build_full-manifest
